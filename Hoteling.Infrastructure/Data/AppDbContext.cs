baee005b88098df20d46edc27abbd7f038092beb
DANYIL DANILIAN <daniliandan100500@gmail.com>
2026-01-04 17:19:05 +0100

    refactor: clean up AppDbContext.cs

--- Hoteling.Infrastructure/Data/AppDbContext.cs
+++ Hoteling.Infrastructure/Data/AppDbContext.cs
@@ -22,10 +22,5 @@ public class AppDbContext : DbContext
             .HasOne(r => r.User)
             .WithMany()
             .HasForeignKey(r => r.UserId);
-
-        // modelBuilder.Entity<Desk>().HasData( //Todo make separate file for seed data in SQL folder
-        //     new Desk { Id = Guid.Parse("d3e4f5a6-b7c8-4d0e-8f2e-3c4d5e6f7a8b"), DeskNumber = "A-001", Floor = 1, HasDualMonitor = true, IsStandingDesk = false },
-        //     new Desk { Id = Guid.Parse("a1b2c3d4-e5f6-4080-9000-010203040506"), DeskNumber = "B-005", Floor = 2, HasDualMonitor = false, IsStandingDesk = true }
-        // );
     }
 }
